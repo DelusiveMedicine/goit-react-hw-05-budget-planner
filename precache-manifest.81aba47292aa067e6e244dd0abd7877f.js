cee3c32b04078d816fbcc02c34aea11b44f9af11
Stacy <enjoyorhate@gmail.com>
2019-12-07 17:06:07 +0200

    Updates

---
+++ precache-manifest.81aba47292aa067e6e244dd0abd7877f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855bcf171bae1fd5056dcff0b351af1a",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "4aaa9788d0a313a13c59",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "e311f4620bc985f94c34",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.c5261b4b.chunk.js"
+  },
+  {
+    "revision": "4aaa9788d0a313a13c59",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.b3917f5b.chunk.js"
+  },
+  {
+    "revision": "8f882ac2b998eaeda7ab",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.ddc82cc2.js"
+  }
+]);
