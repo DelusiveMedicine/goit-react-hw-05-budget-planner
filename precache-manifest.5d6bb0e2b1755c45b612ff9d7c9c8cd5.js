5be91490868a7ab582c253ae1619f6ff917d481b
Stacy <enjoyorhate@gmail.com>
2019-12-08 20:56:04 +0200

    Updates

--- precache-manifest.5d6bb0e2b1755c45b612ff9d7c9c8cd5.js
+++ precache-manifest.5d6bb0e2b1755c45b612ff9d7c9c8cd5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3693cfe9d2a39d6344e215e66fb58db3",
+    "revision": "0447121dcd1176de9f580c46c3b570ec",
     "url": "/goit-react-hw-05-budget-planner/index.html"
   },
   {
-    "revision": "c946d29bcb58a246b745",
+    "revision": "3fd586985a9d6b59f84c",
     "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
   },
   {
-    "revision": "0957f01b18a97255ea9b",
-    "url": "/goit-react-hw-05-budget-planner/static/js/2.a04f8287.chunk.js"
+    "revision": "fc157b5ee4086d74acf9",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.1106413a.chunk.js"
   },
   {
-    "revision": "c946d29bcb58a246b745",
-    "url": "/goit-react-hw-05-budget-planner/static/js/main.2b5a3d04.chunk.js"
+    "revision": "3fd586985a9d6b59f84c",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.ce590338.chunk.js"
   },
   {
     "revision": "8f882ac2b998eaeda7ab",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-05-budget-planner/precache-manifest.cb6fb52246feecf6fa3a784d395c11be.js"
+  "/goit-react-hw-05-budget-planner/precache-manifest.5d6bb0e2b1755c45b612ff9d7c9c8cd5.js"
 );
 
 self.addEventListener('message', (event) => {
